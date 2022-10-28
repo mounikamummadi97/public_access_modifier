afd0862d85b4a8f3cf6031ada2eadc2468f8e1f8
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:02:06 +0530

    first commit

---
+++ public acess modifiers/Program.cs
@@ -0,0 +1,33 @@
+﻿using publicAccessModifier;
+using System;
+namespace publicAccessModifier
+{
+    class Student
+    {
+        public int rollno;
+        public string name;
+        public Student(int rollno, string name)
+        {
+            this.rollno = rollno;
+            this.name = name;
+        }
+        public int getRollNo()
+        { return rollno; }
+        public string getName()
+        { return name; }
+    }
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        Student S = new Student(1, "mona");
+        Student S1= new Student(2, "abc");
+        Console.WriteLine("rollno : {0}", S1.rollno);
+        Console.WriteLine("name : {0}",S1.name);
+        Console.WriteLine("rollno : {0}", S.rollno);
+        Console.WriteLine("name : {0}", S.name);
+        //Console.WriteLine();
+
+    }
+}
